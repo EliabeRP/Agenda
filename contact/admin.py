a6cd0029450a0f578c84431cfa4c493ff4083397
EliabeRP <eliabe.rodrgu3s@gmail.com>
2023-05-12 00:55:54 -0300

    creating contact model

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from contact.models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone', )
+    ordering = ('-id', )
+    search_fields = ('id', 'first_name', 'last_name', 'phone', )
+    list_per_page = 2
+    list_max_show_all = 200
+    list_display_links = ('id', 'first_name', 'phone', )
